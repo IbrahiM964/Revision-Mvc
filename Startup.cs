a4bbf06e6b638dce52e4f338391b6242c6a688bf
IbrahiM964 <109480467+IbrahiM964@users.noreply.github.com>
2022-07-18 10:04:16 +0200

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RevisionMVCENTF.Startup))]
+namespace RevisionMVCENTF
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
